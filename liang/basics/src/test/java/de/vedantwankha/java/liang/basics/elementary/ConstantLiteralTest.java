30abf086c9836179fc30c8310946893cb075ee85
Vedant Wankhade <vedantwankhade17@gmail.com>
2024-10-20 22:17:57 +0530

    literals, types

---
+++ liang/basics/src/test/java/de/vedantwankha/java/liang/basics/elementary/ConstantLiteralTest.java
@@ -0,0 +1,10 @@
+package de.vedantwankha.java.liang.basics.elementary;
+
+import org.junit.Test;
+
+public class ConstantLiteralTest {
+    @Test
+    public void testLiteral() {
+        ConstantLiteral.literal();
+    }
+}
